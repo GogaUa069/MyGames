35b424144657ffb911011b639f5e0f9b48b7fd19
Goga_ua <pavlenkoegor1412@gmail.com>
2025-08-31 11:59:02 +0200

    GC 4.2.2 ALFA

--- GC_4.2.2_ALFA.py
+++ GC_4.2.2_ALFA.py
@@ -1,3 +1,5 @@
+# Version 4.2.2 ALFA
+
 import time
 from colorama import Fore, Style
 from random import randint, shuffle, choice
@@ -46,6 +48,7 @@ class SystemPrompt:
 
     def farewell(self, communicate="Goodbye! :)"):
         print(self.get_colored_text(f">>> {communicate}", "LIGHTCYAN_EX"))
+        time.sleep(0.5)
 
     def pass_func(self):
         return
@@ -54,7 +57,7 @@ class SystemPrompt:
 system = SystemPrompt()
 
 
-class GameIntro:  # COOKED
+class GameIntro:
     INTRO_SOUNDTRACK = pygame.Sound("Sounds/IntroMusic.wav")
 
     @staticmethod
@@ -89,8 +92,11 @@ class GameIntro:  # COOKED
     def intro(self):
         self.INTRO_SOUNDTRACK.play()
         self.my_accounts()
-        self.show_game_banner("Guess Code 4.2.0 ALFA")  # UPDATES UPDATES UPDATES UPDATES UPDATES UPDATES
+        self.show_game_banner("Guess Code 4.2.2 ALFA")  # UPDATES UPDATES UPDATES UPDATES UPDATES UPDATES
         self.loading()
+
+
+game_intro = GameIntro()
 
 
 class MenuPattern:
@@ -100,7 +106,7 @@ class MenuPattern:
         self.answer = str()
 
     def show_menu(self):
-        print(system.get_colored_text(f">>> {self.header}", "LIGHTRED_EX"))
+        print(system.get_colored_text(f"\n>>> {self.header}", "LIGHTRED_EX"))
         for indx, option in enumerate(self.options):
             print(system.get_colored_text(f"{indx + 1}. {option.name}", "BLUE"))
 
@@ -118,6 +124,39 @@ class MenuPattern:
             self.match_answer()
 
 
+class Option:
+    def __init__(self, name: str, func, variants: tuple):
+        self.name = name
+        self.func = func
+        self.variants = variants
+
+    def usage(self):
+        self.func()
+
+
+class SoundtrackOption:
+    def __init__(self, name: str, path, variants: tuple):
+        self.name = name
+        self.path = path
+        self.variants = variants
+
+    def usage(self):
+        Soundtracks.play_soundtrack(self.path)
+
+
+class SoundOption:
+    def __init__(self, name: str, sound, variants: tuple, is_sound: bool):
+        self.name = name
+        self.sound = sound
+        self.variants = variants
+        self.is_sound = is_sound
+
+    def usage(self):
+        flag = sounds.toggle_sound()
+        print(self.sound[flag]["COMMUNICATE"])
+        sound = self.sound[flag]
+        sound["SOUND"].play(maxtime=sound["MAXTIME"])
+
 class Soundtracks:
     HEADER = "Soundtracks"
 
@@ -126,11 +165,12 @@ class Soundtracks:
     ELECTRIC_JAZZ = "Music/ElectricJazz.wav"
 
     def __init__(self):
-        self.JAZZ1 = ...
-        self.JAZZ2 = ...
-        self.JAZZ3 = ...
-        self.TURN_OFF = ...
-        self.BACK = ...
+        self.JAZZ1 = SoundtrackOption("Casual Jazz", self.CASUAL_JAZZ, ("CASUAL", "JAZZ 1", "1"))
+        self.JAZZ2 = SoundtrackOption("Cool Jazz", self.COOL_JAZZ, ("COOL", "JAZZ 2", "2"))
+        self.JAZZ3 = SoundtrackOption("Electric Jazz", self.ELECTRIC_JAZZ, ("ELECTRIC", "JAZZ 3", "3"))
+        self.TURN_OFF = Option("Turn Off", self.turn_off_music, ("TURN OFF", "OFF", "4"))
+        self.BACK = Option("Back - Audio", system.pass_func, ("BACK", "AUDIO", "5"))
+
         self.OPTIONS = (self.JAZZ1, self.JAZZ2, self.JAZZ3, self.TURN_OFF, self.BACK)
 
     @staticmethod
@@ -141,57 +181,100 @@ class Soundtracks:
         except pygame.error:
             system.error(f"Can't find soundtrack at path {path}")
 
+    @staticmethod
+    def turn_off_music():
+        pygame.mixer.music.stop()
+        print(system.get_colored_text("Music is turned Off", "LIGHTRED_EX"))
+
     def __call__(self, *args, **kwargs):
-        ...
+        soundtracks_menu = MenuPattern(self.HEADER, self.OPTIONS)
+        soundtracks_menu.select_answer()
+
+
+soundtracks = Soundtracks()
 
 
 class Sounds:
     HEADER = "Sounds"
     IS_SOUND = True
 
-    VICTORY = pygame.Sound("Sounds/YouWinGoga.wav")  # MAXTIME = 2350
-    FAILURE = pygame.Sound("Sounds/YouLostGoga.wav")  # MAXTIME = 2500
+    SOUNDS = {True: {"SOUND": pygame.Sound("Sounds/YouWinGoga.wav"), "MAXTIME": 2350,
+                     "COMMUNICATE": system.get_colored_text("Sounnds're turned On", "LIGHTGREEN_EX")},
+              False: {"SOUND": pygame.Sound("Sounds/YouLostGoga.wav"), "MAXTIME": 2500,
+                    "COMMUNICATE": system.get_colored_text("Sounds're turned Off", "LIGHTRED_EX")}
+              }
 
     def __init__(self):
-        self.TOGGLE = ...
-        self.BACK = ...
+        self.TOGGLE = SoundOption("Turn On/Off", self.SOUNDS, ("ON", "OFF", "1"), self.IS_SOUND)
+        self.BACK = Option("Back - Audio", system.pass_func, ("BACK", "AUDIO", "2"))
+
         self.OPTIONS = (self.TOGGLE, self.BACK)
 
+    def toggle_sound(self):
+        self.IS_SOUND = not self.IS_SOUND
+        return self.IS_SOUND
+
     def __call__(self, *args, **kwargs):
-        ...
+        sounds_menu = MenuPattern(self.HEADER, self.OPTIONS)
+        sounds_menu.select_answer()
+
+
+sounds = Sounds()
 
 
 class Audio:
     HEADER = "Audio"
 
     def __init__(self):
-        self.SOUNDTRACKS = ...
-        self.SOUNDS = ...
-        self.BACK = ...
+        self.SOUNDTRACKS = Option(soundtracks.HEADER, soundtracks, ("SOUNDTRACKS", "1"))
+        self.SOUNDS = Option(sounds.HEADER, sounds, ("SOUNDS", "2"))
+        self.BACK = Option("Back - Settings", system.pass_func, ("BACK", "SETTINGS", "3"))
         self.OPTIONS = (self.SOUNDTRACKS, self.SOUNDS, self.BACK)
 
     def __call__(self, *args, **kwargs):
-        ...
+        audio_menu = MenuPattern(self.HEADER, self.OPTIONS)
+        audio_menu.select_answer()
+
+
+audio = Audio()
 
 
 class Settings:
     HEADER = "Settings"
 
     def __init__(self):
-        self.RULES = ...
-        self.AUDIO = ...
-        self.BACK = ...
+        self.RULES = Option("Rules", system.rules, ("RULES", "1"))
+        self.AUDIO = Option(audio.HEADER, audio, ("AUDIO", "2"))
+        self.BACK = Option("Back - Main Menu", system.pass_func, ("BACK", "MAIN MENU", "3"))
         self.OPTIONS = (self.RULES, self.AUDIO, self.BACK)
 
     def __call__(self, *args, **kwargs):
-        ...
+        settings_menu = MenuPattern(self.HEADER, self.OPTIONS)
+        settings_menu.select_answer()
+
+
+settings = Settings()
 
 
 class MainMenu:
     HEADER = "Main Menu"
 
     def __init__(self):
-        self.PLAY = ...
-        self.SETTINGS = ...
-        self.QUIT = ...
+        self.PLAY = Option("Play", system.coming_soon, ("PLAY", "P", "1"))
+        self.SETTINGS = Option(settings.HEADER, settings, ("SETTINGS", "S", "2"))
+        self.QUIT = Option("Quit", system.farewell, ("QUIT", "Q", "3"))
         self.OPTIONS = (self.PLAY, self.SETTINGS, self.QUIT)
+
+    def __call__(self, *args, **kwargs):
+        main_menu_ = MenuPattern(self.HEADER, self.OPTIONS)
+        main_menu_.select_answer()
+
+
+main_menu = MainMenu()
+
+def game():
+    game_intro.intro()
+    main_menu()
+
+
+game()
